2ac4f3bcd571113a42cafc6ca3400c3647733a7b
unknown <sak.dhormale@gmail.com>
2025-03-11 23:38:43 +0530

    files added

---
+++ backend/app.py
@@ -0,0 +1,33 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+import mysql.connector
+
+app = Flask(__name__)
+CORS(app)
+# Database Connection
+db = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="123456",
+    database="aarthika"
+)
+cursor = db.cursor()
+
+# Check if the connection was successful
+try:
+    db.ping(reconnect=True)
+    print("Connected to the database.")
+except mysql.connector.Error as err:
+    print("Error connecting to MySQL:", err)
+
+
+
+# Home Route
+@app.route('/', methods=['GET'])
+def home():
+    return jsonify({"message": "Welcome to Aarthika API!"})
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
